ece2054fb3f865ec52394e56aa0faeda7189b85b
dsilvaian <66319598+dsilvaian@users.noreply.github.com>
2021-06-28 00:36:23 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This Bootcamp conducted by ShapeAI was very informative and knowlegable
+        towards the new technologies in web dev
+      </p>
+    </div>
+  );
+}
+
+export default Info;
